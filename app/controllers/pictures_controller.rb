a5a6855b90c8b0b18a37492dac681f8f468ba8ba
Luiz Cláudio <luiz.lopes@codeminer42.com>
2019-03-15 16:29:08 -0300

    add and show pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,35 @@
 class PicturesController < ApplicationController
+  before_action :find_picture, only: [:show, :edit, :update, :destroy]
 
-    def index
+  def index
+    @pictures = Picture.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    if @picture.save
+      redirect_to @picture, notice: "Picture posted!"
+    else
+      render 'new'
     end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :description)
+  end
+
+  def find_picture
+    @picture = Picture.find(params[:id])
+  end
+
 end
